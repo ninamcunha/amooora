3a1b5d33293a6f6359cebc9f63f04295c4a6f25c
Andre Menezes <djdedemenezes@gmail.com>
2025-03-24 11:45:10 -0300

    add function to load lda model from pickle file

---
+++ amooora/ml_logic/registry.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import pickle
+from pathlib import Path
+
+from colorama import Fore, Style
+
+def load_model(): # o que retorna aqui?
+    """
+    Return saved model:
+
+    Return None (but do not Raise) if no model is found
+    """
+
+    # define model path
+    model_filepath = Path("pkl/model.pkl")
+    print(Fore.BLUE + f"\nLoad latest model from local registry (pkl folder)..." + Style.RESET_ALL)
+
+
+    # check if there is a file at MODEL_LOCATION
+    if not model_filepath.is_file():
+        print(f"❌ There is no file at {model_filepath}")
+    # Return none if there is no file
+        return None
+
+    # Return model if there is a file
+    with open(model_filepath, 'rb') as f:
+        lda = pickle.load(f)
+
+    print("✅ Model loaded from local disk")
+
+    return lda
+
+
+load_model()
